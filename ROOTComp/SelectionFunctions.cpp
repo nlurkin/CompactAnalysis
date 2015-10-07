5049593841781be13bc427456fea47e33da8b275
nlurkin <nicolas.lurkin@gmail.com>
2015-10-07 15:07:31 +0200

    Automatic commit

--- ROOTComp/SelectionFunctions.cpp
+++ ROOTComp/SelectionFunctions.cpp
@@ -634,21 +634,21 @@ int pi0d_goodClusters_tight(NRecoParticle &xParticle, ROOTPhysicsEvent &event){
 		propPos = propagateAfter(rootGeom.Lkr.z, x);
 		distance = distance2D(propPos, c.position);
 		//if(options.isOptDebug()) cout << "\t\tR_LKr_x :\t\t" << distance << "\t > 50 : ++" << endl;
-		//if(distance>50) cond++;
-		cond++;
+		if(distance>20) cond++;
+		//cond++;
 
 		// separation from e+ e- impact point >10cm
 		propPos = propagateAfter(rootGeom.Lkr.z, ep);
 		distance = distance2D(propPos, c.position);
 		//if(options.isOptDebug()) cout << "\t\tR_LKr_e+ :\t\t" << distance << "\t > 20 : ++" << endl;
-		//if(distance>20) cond++;
-		cond++;
+		if(distance>20) cond++;
+		//cond++;
 
 		propPos = propagateAfter(rootGeom.Lkr.z, em);
 		distance = distance2D(propPos, c.position);
 		//if(options.isOptDebug()) cout << "\t\tR_LKr_e- :\t\t" << distance << "\t > 20 : ++" << endl;
-		//if(distance>20) cond++;
-		cond++;
+		if(distance>20) cond++;
+		//cond++;
 
 		// separation from undeflected e+ e- trajectories >20cm
 		propPos = propagate(rootGeom.Lkr.z, rawEvent.track[ep.trackID].bDetPos, rawEvent.track[ep.trackID].bMomentum);
