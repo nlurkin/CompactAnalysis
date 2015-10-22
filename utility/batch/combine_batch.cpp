2a4b8d212ac74cd454df3a0db6c70b0eec1c001e
nlurkin <nicolas.lurkin@gmail.com>
2015-10-22 19:16:29 +0200

    Automatic commit

--- utility/batch/combine_batch.cpp
+++ utility/batch/combine_batch.cpp
@@ -522,8 +522,8 @@ void fillHistos(vector<TH1D*> *d, vector<TH2D*> *vMap, ROOTPhysicsEvent *evt, RO
 	propPos = propagateBefore(rootGeom->Dch[0].PosChamber.z, corrEvent->pTrack[evt->ep.parentTrack]);
 	propPos2 = propagateBefore(rootGeom->Dch[0].PosChamber.z, corrEvent->pTrack[evt->em.parentTrack]);
 
-	//if(distance2D(propPos, TVector3(0,0,0))<15) return;
-	//if(distance2D(propPos2, TVector3(0,0,0))<15) return;
+	if(distance2D(propPos, TVector3(0,0,0)) < 20) return;
+	if(distance2D(propPos2, TVector3(0,0,0))< 20) return;
 
 	d->at(++i)->Fill(evt->kaon.P.M(), weight);
 
