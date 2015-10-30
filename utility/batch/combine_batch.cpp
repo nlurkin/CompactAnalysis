4d50ccf7bf6338ce90f38d35ad417a1c3843dbf0
nlurkin <nicolas.lurkin@gmail.com>
2015-10-30 23:57:53 +0100

    Automatic commit

--- utility/batch/combine_batch.cpp
+++ utility/batch/combine_batch.cpp
@@ -540,8 +540,8 @@ void fillHistos(vector<TH1D*> *d, vector<TH2D*> *vMap, ROOTPhysicsEvent *evt, RO
 //	}
 	int miny = 20;
 	int maxy = 30;
-	int minx = -15;
-	int maxx = -10;
+	int minx = -10;
+	int maxx = -5;
 
 	if(propPos.Y()<30 && propPos.Y()>-30 && propPos.X()<30 && propPos.X()>-30){
 		//Do fine selection
