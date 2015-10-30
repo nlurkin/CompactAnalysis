9d051bb051ad7b23d05f906a958e9151b9475c96
nlurkin <nicolas.lurkin@gmail.com>
2015-10-30 15:58:57 +0100

    Automatic commit

--- utility/batch/combine_batch.cpp
+++ utility/batch/combine_batch.cpp
@@ -538,8 +538,8 @@ void fillHistos(vector<TH1D*> *d, vector<TH2D*> *vMap, ROOTPhysicsEvent *evt, RO
 //		fitBrch.selEvents--;
 //		return;
 //	}
-	int miny = 0;
-	int maxy = 10;
+	int miny = -10;
+	int maxy = 0;
 	int minx = -30;
 	int maxx = -25;
 
