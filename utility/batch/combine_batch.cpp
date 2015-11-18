308e9f8820e4ef0fe42d7c5e32f095ae8ac3be47
nlurkin <nicolas.lurkin@gmail.com>
2015-11-18 13:04:00 +0100

    Automatic commit

--- utility/batch/combine_batch.cpp
+++ utility/batch/combine_batch.cpp
@@ -26,6 +26,15 @@ static vector< vector<TH2D*> > *dMap, *dSigMap;
 ROOTRawEvent *xxx = new ROOTRawEvent();
 ROOTRawEvent &rawEvent = *xxx;
 
+struct square{
+	square(int y1, int y2, int x1, int x2):
+	miny(y1), maxy(y2), minx(x1), maxx(x2){}
+	int miny;
+	int maxy;
+	int minx;
+	int maxx;
+};
+
 /***************************
  * Mandatory from header
  * Opening/Closing files
@@ -539,25 +548,31 @@ void fillHistos(vector<TH1D*> *d, vector<TH2D*> *vMap, ROOTPhysicsEvent *evt, RO
 //		return;
 //	}
 
-	int miny = 10;
-	int maxy = 30;
-	int minx = 10;
-	int maxx = 30;
+	vector<square> sq = {
+			square(10, 30, -30, -10), square(-10, 10, -30, -10), square(-30, -10, -30, -10),
+			square(10, 30, -10, 10), square(-10, 10, -10, 10), square(-30, -10, -10, -10),
+			square(10, 30, 10, 30), square(-10, 10, 10, 30), square(-30, -10, 10, 30),
+	};
+	vector<int> incSq = {1};
 
-	//if(propPos.Y()<30 && propPos.Y()>-30 && propPos.X()<30 && propPos.X()>-30){
+	if(propPos.Y()<30 && propPos.Y()>-30 && propPos.X()<30 && propPos.X()>-30){
 		//Do fine selection
-		if( propPos.Y()>maxy || propPos.Y()<miny || (propPos.X()>maxx || propPos.X()<minx) ){
-			fitBrch.selEvents--;
-			return;
+		for(auto sqID : incSq){
+			if( propPos.Y()>sq[sqID].maxy|| propPos.Y()<sq[sqID].miny || (propPos.X()>sq[sqID].maxx || propPos.X()<sq[sqID].minx) ){
+				fitBrch.selEvents--;
+				return;
+			}
 		}
-	//}
-	//if(propPos2.Y()<30 && propPos2.Y()>-30 && propPos2.X()<30 && propPos2.X()>-30){
+	}
+	if(propPos2.Y()<30 && propPos2.Y()>-30 && propPos2.X()<30 && propPos2.X()>-30){
 		//Do fine selection
-		if( propPos2.Y()>maxy || propPos2.Y()<miny || (propPos2.X()>maxx || propPos2.X()<minx) ){
-			fitBrch.selEvents--;
-		return;
+		for(auto sqID : incSq){
+			if( propPos2.Y()>sq[sqID].maxy || propPos2.Y()<sq[sqID].miny || (propPos2.X()>sq[sqID].maxx || propPos2.X()<sq[sqID].minx) ){
+				fitBrch.selEvents--;
+				return;
+			}
 		}
-	//}
+	}
 //	if(distance2D(propPos, TVector3(0,0,0)) < 30){
 //		fitBrch.selEvents--;
 //		return;
