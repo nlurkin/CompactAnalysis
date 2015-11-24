23fc7296fb764db70ba33b19e0989ec3ffb778ba
nlurkin <nicolas.lurkin@gmail.com>
2015-11-24 12:38:51 +0100

    Automatic commit

--- utility/batch/combine_batch.cpp
+++ utility/batch/combine_batch.cpp
@@ -539,7 +539,13 @@ void fillHistos(vector<TH1D*> *d, vector<TH2D*> *vMap, ROOTPhysicsEvent *evt, RO
 	int iMap=-1;
 	TVector3 propPos, propPos2, propPos3;
 
-	//Cut 12cm on DCH1
+	propPos = propagateBefore(rootGeom->Dch[0].PosChamber.z, corrEvent->pTrack[evt->ep.parentTrack]);
+	propPos2 = propagateBefore(rootGeom->Dch[0].PosChamber.z, corrEvent->pTrack[evt->em.parentTrack]);
+	if(distance2D(propPos, TVector3(0,0,0))<20 && distance2D(propPos2, TVector3(0,0,0))<20 ){
+		fitBrch.selEvents--;
+		return;
+	}
+	/*//Cut 12cm on DCH1
 	propPos = propagateBefore(rootGeom->Dch[0].PosChamber.z, corrEvent->pTrack[evt->ep.parentTrack]);
 	propPos2 = propagateBefore(rootGeom->Dch[0].PosChamber.z, corrEvent->pTrack[evt->em.parentTrack]);
 	if(distance2D(propPos, TVector3(0,0,0))<15){
@@ -572,7 +578,7 @@ void fillHistos(vector<TH1D*> *d, vector<TH2D*> *vMap, ROOTPhysicsEvent *evt, RO
 	if(distance2D(propPos2, TVector3(0,0,0))<20){
 		fitBrch.selEvents--;
 		return;
-	}
+	}*/
 
 
 	/*vector<square> sq = {
