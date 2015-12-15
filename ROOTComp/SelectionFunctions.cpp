9d92c1ac24582845505a132de4aca121d9baff0e
nlurkin <nicolas.lurkin@gmail.com>
2015-12-15 18:26:11 +0100

    Automatic commit

--- ROOTComp/SelectionFunctions.cpp
+++ ROOTComp/SelectionFunctions.cpp
@@ -283,7 +283,8 @@ int pid(int &xCandidate, TLorentzVector &gamma, OptionsParser::ESelectionType t)
 
 	cout << "################################################" << tem.E() << " " << t1ep.E()<< endl;
 	cout << (tem.E()-t1ep.E()) << " " << Mpi0 << " " << Mpi0*(1-x1) << endl;
-	double y1 = 2*(tem.E()-t1ep.E())/(Mpi0*(1-x1));
+	//double y1 = 2*(tem.E()-t1ep.E())/(Mpi0*(1-x1));
+	double y1 = 2*ee1*(t1ep-tem)/(pow(Mpi0,2)*(1-x1));
 	double y2 = 2*(tem.E()-t2ep.E())/(Mpi0*(1-x2));
 
 	if(t==OptionsParser::K2PI){
