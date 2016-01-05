75fc4a1321ce3afa9431a4758241a0b2911072df
nlurkin <nicolas.lurkin@gmail.com>
2016-01-05 17:17:33 +0100

    Add interface for Datasamples and mc samples

---
+++ utility/common/Interface/DataSample.cpp
@@ -0,0 +1,19 @@
+/*
+ * DataSample.cpp
+ *
+ *  Created on: Jan 4, 2016
+ *      Author: nlurkin
+ */
+
+#include "DataSample.h"
+
+DataSample::DataSample() :
+		fTestA(0), fFactor(1) {
+	// TODO Auto-generated constructor stub
+
+}
+
+DataSample::~DataSample() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ utility/common/Interface/DataSample.h
@@ -0,0 +1,37 @@
+/*
+ * DataSample.h
+ *
+ *  Created on: Jan 4, 2016
+ *      Author: nlurkin
+ */
+
+#ifndef COMMON_SAMPLES_DATASAMPLE_H_
+#define COMMON_SAMPLES_DATASAMPLE_H_
+
+class DataSample{
+public:
+	DataSample();
+
+	virtual ~DataSample();
+
+	double getTestA() const {
+		return fTestA;
+	}
+
+	void setTestA(double testA) {
+		fTestA = testA;
+	}
+
+	double getFactor() const {
+		return fFactor;
+	}
+
+	void setFactor(double factor) {
+		fFactor = factor;
+	}
+protected:
+	double fTestA;
+	double fFactor;
+};
+
+#endif /* COMMON_SAMPLES_DATASAMPLE_H_ */
