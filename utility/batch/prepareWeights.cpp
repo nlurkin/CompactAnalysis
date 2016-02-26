8dc96d54c04c5e937b5dda2f93ac256be56d7d49
nlurkin <nicolas.lurkin@gmail.com>
2016-02-26 15:48:49 +0100

    Style

--- utility/batch/prepareWeights.cpp
+++ utility/batch/prepareWeights.cpp
@@ -21,9 +21,9 @@ using namespace std;
 map<int, double> mcRunMap, dataRunMap;
 void buildRatioMap();
 void readConfig(TString confFile);
-void buildRunMap(TFile *fd, map<int,double> &runMap, bool usePk);
+void buildRunMap(TFile *fd, map<int, double> &runMap, bool usePk);
 
-void readConfig(TString confFile){
+void readConfig(TString confFile) {
 	vector<TString> mcFileNames;
 	vector<TString> dataFileNames;
 	bool usePk;
@@ -32,40 +32,44 @@ void readConfig(TString confFile){
 	ifstream listFile(confFile.Data());
 
 	string line;
-	while(getline(listFile,line)){
+	while (getline(listFile, line)) {
 		TString tline(line);
-		if(tline.BeginsWith("#")) continue;
-		TString key(tline(0,tline.First('=')));
-		TString values(tline(tline.First('=')+1, 100000));
+		if (tline.BeginsWith("#"))
+			continue;
+		TString key(tline(0, tline.First('=')));
+		TString values(tline(tline.First('=') + 1, 100000));
 		TObjArray* tok = values.Tokenize(" ");
 		cout << tline << endl;
-		for(int i=0; i<tok->GetEntries(); ++i){
-			TString entry(((TObjString*)tok->At(i))->GetString());
-			cout << entry << endl;
-			if(key.CompareTo("mcfiles")==0) mcFileNames.push_back(entry);
-			else if(key.CompareTo("datafiles")==0) dataFileNames.push_back(entry);
-			else if(key.CompareTo("usepk")==0) usePk = entry.CompareTo("true", TString::kIgnoreCase) ? true : false;
+		for (int i = 0; i < tok->GetEntries(); ++i) {
+			TString entry(((TObjString*) tok->At(i))->GetString());
+			if (key.CompareTo("mcfiles") == 0)
+				mcFileNames.push_back(entry);
+			else if (key.CompareTo("datafiles") == 0)
+				dataFileNames.push_back(entry);
+			else if (key.CompareTo("usepk") == 0)
+				usePk = entry.CompareTo("true", TString::kIgnoreCase) == 0 ?
+						true : false;
 			delete tok->At(i);
 		}
 	}
 
 	TFile *fd;
-	for(unsigned int i=0; i<mcFileNames.size(); ++i){
+	for (unsigned int i = 0; i < mcFileNames.size(); ++i) {
 		fd = TFile::Open(mcFileNames[i], "READ");
 		buildRunMap(fd, mcRunMap, usePk);
 		fd->Close();
 	}
-	for(unsigned int i=0; i<dataFileNames.size(); ++i){
+	for (unsigned int i = 0; i < dataFileNames.size(); ++i) {
 		fd = TFile::Open(dataFileNames[i], "READ");
 		buildRunMap(fd, dataRunMap, false);
 		fd->Close();
 	}
 }
 
-void buildRunMap(TFile *fd, map<int,double> &runMap, bool usePk){
+void buildRunMap(TFile *fd, map<int, double> &runMap, bool usePk) {
 	//Get the TTree
 	//Input
-	TTree *t = (TTree*)fd->Get("event");
+	TTree *t = (TTree*) fd->Get("event");
 	ROOTBurst *burstBrch = new ROOTBurst();
 	ROOTCorrectedEvent *corrBrch = new ROOTCorrectedEvent();
 	vector<bool> *cutsPass = 0;
@@ -89,32 +93,36 @@ void buildRunMap(TFile *fd, map<int,double> &runMap, bool usePk){
 		else
 			weight = 1.;
 
-		if(runMap.count(burstBrch->nrun)>0) runMap[burstBrch->nrun] += 1.*weight;
-		else runMap.insert(make_pair(burstBrch->nrun, 1.*weight));
+		if (runMap.count(burstBrch->nrun) > 0)
+			runMap[burstBrch->nrun] += 1. * weight;
+		else
+			runMap.insert(make_pair(burstBrch->nrun, 1. * weight));
 	}
 }
 
-void buildRatioMap(){
-	map<int,double>::iterator it;
+void buildRatioMap() {
+	map<int, double>::iterator it;
 
-	map<int,double> ratio(mcRunMap);
+	map<int, double> ratio(mcRunMap);
 
-	for(it=dataRunMap.begin(); it!=dataRunMap.end(); ++it){
-		ratio[it->first] /=it->second;
+	for (it = dataRunMap.begin(); it != dataRunMap.end(); ++it) {
+		ratio[it->first] /= it->second;
 	}
 
 	ofstream fd;
 	fd.open("pi0dalitz_weights.dat");
-	for(it=ratio.begin(); it!=ratio.end(); ++it){
-		cout << "Run " << it->first << ": " << it->second << "(" << mcRunMap[it->first] << "/" << dataRunMap[it->first] << endl;
-		fd << it->first << " " << it->second << " " << mcRunMap[it->first] << " " << dataRunMap[it->first] << endl;
+	for (it = ratio.begin(); it != ratio.end(); ++it) {
+		cout << "Run " << it->first << ": " << it->second << "("
+				<< mcRunMap[it->first] << "/" << dataRunMap[it->first] << endl;
+		fd << it->first << " " << it->second << " " << mcRunMap[it->first]
+				<< " " << dataRunMap[it->first] << endl;
 	}
 
 	fd.close();
 
 }
 
-void prepareWeights(TString cfgFile){
+void prepareWeights(TString cfgFile) {
 	gSystem->Load("../obj/libmystructs.so");
 
 	readConfig(cfgFile);
@@ -122,6 +130,6 @@ void prepareWeights(TString cfgFile){
 	buildRatioMap();
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 	prepareWeights(argv[1]);
 }
